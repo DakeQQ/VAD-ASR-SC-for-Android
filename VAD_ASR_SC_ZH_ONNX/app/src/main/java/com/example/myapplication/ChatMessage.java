c5a03dec17a67be60f7de38d1a1401d57e14ee69
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-02-18 20:38:49 +0800

    Update ChatMessage.java

--- VAD_ASR_SC_ZH_ONNX/app/src/main/java/com/example/myapplication/ChatMessage.java
+++ VAD_ASR_SC_ZH_ONNX/app/src/main/java/com/example/myapplication/ChatMessage.java
@@ -3,5 +3,5 @@ package com.example.myapplication;
 public record ChatMessage(int type, String content) {
     public static final int TYPE_USER = 0;
     public static final int TYPE_SERVER = 1;
-    public static final int TYPE_System = 2;
+    public static final int TYPE_SYSTEM = 2;
 }
